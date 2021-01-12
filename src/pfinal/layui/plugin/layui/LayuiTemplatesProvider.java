568158f3d7bc50922657a919fdb94354125ada48
Liu XuZhu <xuzhu.liu@avazu.net>
2021-01-12 17:57:31 +0800

    init source code

---
+++ src/pfinal/layui/plugin/layui/LayuiTemplatesProvider.java
@@ -0,0 +1,8 @@
+package pfinal.layui.plugin.layui;
+
+import com.intellij.codeInsight.template.impl.DefaultLiveTemplatesProvider;
+import org.jetbrains.annotations.Nullable;
+
+public class LayuiTemplatesProvider{
+
+}
